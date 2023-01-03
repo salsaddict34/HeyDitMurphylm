0d8b41947712259eb912e13eba357e936f50dc78
salsaddict34 <salsaddict34@free.fr>
2023-01-03 15:57:27 +0100

    App.js modifications add axios module add context auth and darkMode

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import React, { useState } from "react";
 import './App.css';
+import Header from "./components/Header/Header";
+import Home from "./pages/Home/Home"
+import Register from "./pages/Register/Register";
+import Login from "./pages/Login/Login";
 
 function App() {
+
+  const [items, setItems] = useState([]);
+  const [message, setMessage] = useState('')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header message={message} setMessage={setMessage} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth/register" element={<Register />} />
+        <Route path="/auth/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter >
   );
 }
 
