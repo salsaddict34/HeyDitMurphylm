64da71c67c904acbd285a202afbd3ccf68681170
Louison Delagarde <louison.delagarde@gmail.com>
2023-01-05 11:01:11 +0100

    Add footer logos

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,12 +1,23 @@
 import React from "react"
 import {Link} from "react-router-dom"
+// Import of pictures
+import Facebook from "../../assets/img/facebook.png"
+import Instagram from "../../assets/img/instagram.png"
+import LinkedIn from "../../assets/img/linkedin.png"
 
 export default function Footer () {
     return (
         <div className="App-footer">
-        <Link to={'/'} className='footer--link'>Mentions Légales</Link><br/>
-        <Link to={'/'} className='footer--link'>Contactez-nous !</Link><br/>
-        <Link to={'/'} className='footer--link'>FAQ</Link>
+            <div>
+                <Link to={'/'} className='footer--link'>Mentions Légales</Link><br/>
+                <Link to={'/'} className='footer--link'>Contactez-nous !</Link><br/>
+                <Link to={'/'} className='footer--link'>FAQ</Link>
+            </div>
+            <div>
+                <Link to={'/'}><img src={Facebook} alt="Facebook" height={30}/></Link>
+                <Link to={'/'}><img src={Instagram} alt="Instagram" height={30}/></Link>
+                <Link to={'/'}><img src={LinkedIn} alt="LinkedIn" height={30}/></Link>
+            </div>
         </div>
     )
 }
